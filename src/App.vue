81873ef17f851ff8d427a18f8b104d90e8d3441c
Claas <claas@claas.dev>
2024-12-02 22:18:01 +0100

    Make it jank

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <fieldset
       v-for="step in 3"
       :id="`step-${step}`"
-      class="bg-amber-200 place-content-center justify-items-center target:visible relative hidden group-[:not(:has(:target))]:[&[id='step-1']]:block target:block h-0 [&[id='step-1']]:h-screen target:h-screen"
+      class="bg-amber-200 place-content-center justify-items-center target:visible relative hidden group-[:not(:has(:target))]:first:block target:block h-0 group-[:not(:has(:target))]:first:[&[id='step-1']]:h-screen target:h-screen transition-all duration-500 transition-discrete open:opacity-100 starting:translate-x-full starting:absolute starting:right-full"
     >
       <legend class="contents text-7xl font-bold font-mono">
         <h1>Step {{ step }}</h1>
@@ -17,7 +17,7 @@
         &larr;
       </a>
       <a
-        v-if="step <= 3"
+        v-if="step < 3"
         :href="`#step-${step + 1}`"
         class="rounded-full bg-slate-950 text-emerald-50 p-4 absolute bottom-2 min-w-12 leading-0 aspect-square right-2 place-content-center text-2xl"
       >
